4dbae4db4b17c4d16dfc5aeff5c305b04aa7f465
Western Onzere <minichworks@gmail.com>
2024-05-17 02:00:31 +0300

    Data ingestion complete

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.ElectricityBill import logging
+from src.ElectricityBill.pipelines.pip_01_data_ingestion import DataIngestionPipeline
+
+
+
+COMPONENT_NAME = "Data Ingestion Component"
+try:
+    logging.info(f"# ========= {COMPONENT_NAME} Started ================")
+    pipeline = DataIngestionPipeline()
+    pipeline.main()
+    logging.info(f"# ============= {COMPONENT_NAME} Terminated Successfully ! ==============\n\nx**************x")
+except Exception as e:
+    logging.exception(e)
+    raise e
---
+++ src/ElectricityBill/config/configuration.py
@@ -0,0 +1,39 @@
+from src.ElectricityBill.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH, SCHEMA_FILE_PATH
+from src.ElectricityBill.utils.commons import read_yaml, create_directories, get_size
+from src.ElectricityBill.entity.configuration_entity import DataIngestionConfig
+
+
+# Create a configuration manager class to manage the configurations 
+class ConfigurationManager:
+    def __init__(
+            self,
+            config_filepath = CONFIG_FILE_PATH,
+            params_filepath = PARAMS_FILE_PATH,
+            schema_filepath = SCHEMA_FILE_PATH
+            ):
+        
+        # Initialize the configuration manager 
+        # Read YAML configurations files to initialize configuration parameters 
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
+        
+        # Create necessary directories specified in the configuration
+        create_directories([self.config.artifacts_root])
+
+    def get_data_ingestion_config(self) ->DataIngestionConfig:
+            # Get the data ingestion configuration 
+            config = self.config.data_ingestion
+
+            # Create the data ingestion configuration object 
+            create_directories([config.root_dir])
+
+            # Create and return the Data Ingestion Config object 
+            data_ingestion_config = DataIngestionConfig(
+                root_dir = config.root_dir,
+                source_URL = config.source_URL,
+                local_data_file = config.local_data_file,
+                unzip_dir = config.unzip_dir
+            )
+
+            return data_ingestion_config
